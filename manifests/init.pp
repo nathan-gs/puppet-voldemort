d1d684e8bf1cfd4e6984779a7b295068f22a59bc
Nathan Bijnens <nathan@nathan.gs>
2013-04-29 13:44:38 +0200

    install voldemort

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class voldemort {
 
     package { 'voldemort':
         provider	=> rpm,
-        source		=> 'https://dl.dropboxusercontent.com/u/25821613/rpms/voldemort-1.3.0-dc.x86_64.rpm',
-        ensure      => '1.3.0-dc'
+        source		=> 'https://dl.dropboxusercontent.com/u/25821613/rpms/voldemort-1.3.0-dc0.x86_64.rpm',
+        ensure      => '1.3.0-dc0'
     }
 
 
